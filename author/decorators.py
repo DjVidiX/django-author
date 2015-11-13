e41985fa87d67548dc75b8726be0183474e64e29
DjVidiX <djvidix.ms@gmail.com>
2015-11-13 19:47:11 +0100

    fixes related_name problem

--- author/decorators.py
+++ author/decorators.py
@@ -41,8 +41,8 @@ def with_author(cls):
 
     user_model = _get_user_model()
     verbose_name_plural = cls._meta.object_name
-    created_by = models.ForeignKey(user_model, verbose_name=_('author'), related_name='%s_create' % verbose_name_plural.lower(), null=True, blank=True)
-    updated_by = models.ForeignKey(user_model, verbose_name=_('last updated by'), related_name='%s_update' % verbose_name_plural.lower(), null=True, blank=True)
+    created_by = models.ForeignKey(user_model, verbose_name=_('author'), related_name='%(class)s_related_author', null=True, blank=True)
+    updated_by = models.ForeignKey(user_model, verbose_name=_('last updated by'), related_name='%(class)s_related_modifier', null=True, blank=True)
 
     if not hasattr(cls, settings.AUTHOR_CREATED_BY_FIELD_NAME):
         cls.add_to_class(settings.AUTHOR_CREATED_BY_FIELD_NAME, created_by)
